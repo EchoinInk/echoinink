1f5f778bedcdff54aba28fdf2805c3933a21e7f4
EchoinInk <alex@echoin.ink>
2026-05-19 20:58:45 +1200

    Adjust layout and responsiveness

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -24,8 +24,7 @@ export function Hero() {
       </div>
 
       {/* Layer 3: Content/text - anchored near lower portion */}
-      <div className="absolute bottom-0 left-0 right-0 z-10 flex flex-col items-center text-center px-6 pb-16 md:pb-24" style={{ marginTop: '14vh' }}>
-        <HeroHeading />
+<div className="absolute inset-x-0 bottom-0 z-10 flex flex-col items-center text-center px-6 pb-8 md:pb-12">        <HeroHeading />
         <HeroCTA />
       </div>
 
