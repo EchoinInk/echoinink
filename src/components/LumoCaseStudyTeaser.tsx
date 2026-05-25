b60609bc71eb39cf2aead55a8394a15c68795f43
EchoinInk <alex@echoin.ink>
2026-05-26 06:04:04 +1200

    Refine motion and transitions

--- src/components/LumoCaseStudyTeaser.tsx
+++ src/components/LumoCaseStudyTeaser.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import lumocloudform from '@/assets/lumocasestudy.png';
+import lumocloudform from '@/assets/ei-lumofeatured-bg.png';
 import { Section } from '@/components/layout/Section';
 import { Container } from '@/components/layout/Container';
 import { 
