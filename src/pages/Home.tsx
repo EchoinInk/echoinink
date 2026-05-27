738e3ce1a7ceea984e5f0d8409e8ecf0cc1ec000
EchoinInk <alex@echoin.ink>
2026-05-28 01:34:20 +1200

    Refine interface atmosphere

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import { PageShell } from '@/components/system';
 import { Hero } from '@/components/hero/Hero';
 import { Philosophy } from '@/components/Philosophy';
-import { Pathways } from '@/components/Pathways';
+import { <WhatWeCreate></WhatWeCreate> } from '@/components/Pathways';
 import { LumoCaseStudyTeaser } from '@/components/LumoCaseStudyTeaser';
 import { EmergingSystems } from '@/components/EmergingSystems';
 import { ClosingSection } from '@/components/ClosingSection';
