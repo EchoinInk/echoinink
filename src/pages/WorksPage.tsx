c48bef4b3594b5a6fa215b5dc12c44c53c5116ce
EchoinInk <alex@echoin.ink>
2026-06-04 08:35:29 +1200

    Enhance overall experience

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import { motion } from 'framer-motion';
 import { PageShell } from '@/components/layout/PageShell';
 import { Container } from '@/components/layout/Container';
-import { EditorialHero } from '@/components/hero/EditorialHero';
+import { EditorialHero } from '@/components/sections/EditorialHero';
 import { WorkFilterBar } from '@/components/works/WorkFilterBar';
 import { WorksGrid } from '@/components/works/WorksGrid';
 import { ProjectCTA } from '@/components/works/ProjectCTA';
