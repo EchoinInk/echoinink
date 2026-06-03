f43e1d730e379baa666a02cb59ecea261a3a6c92
EchoinInk <alex@echoin.ink>
2026-06-03 13:50:14 +1200

    Improve visual composition

--- src/pages/Studio.tsx
+++ src/pages/Studio.tsx
@@ -53,7 +53,7 @@ function StudioSectionRail({
       </span>
 
       <span
-        className="block h-px w-16"
+        className="block h-px w-22"
         style={{
           background:
             'linear-gradient(90deg, rgb(var(--ei-electric-cyan-rgb) / 0.28), rgb(var(--ei-aurora-blue-rgb) / 0.85))',
