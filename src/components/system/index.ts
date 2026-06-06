114b322ec68eac73e7468c899df4c5d621ef1b39
EchoinInk <alex@echoin.ink>
2026-06-07 04:28:54 +1200

    Improve hero presentation

--- src/components/system/index.ts
+++
@@ -1,5 +0,0 @@
-export { PageSection } from '../layout/PageSection';
-export type { PageSectionVariant } from '../layout/PageSection';
-export { PageShell } from '../layout/PageShell';
-export type { PageAtmosphere } from '../layout/PageShell';
-export { AtmosphericBridge } from '../atmosphere/AtmosphericBridge';
