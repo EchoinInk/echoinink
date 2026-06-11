a32d8171176f9af39eff96f3d990e8b61581f436
EchoinInk <alex@echoin.ink>
2026-06-12 11:05:12 +1200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App() {
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <a
           href="#main-content"
-          className="ei-type-color-primary sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[9999] focus:px-4 focus:py-2 focus:bg-white/10 focus:rounded focus:outline-none focus:ring-2 focus:ring-white/55"
+          className="ei-type-color-primary sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[9999] focus:px-4 focus:py-2 focus:bg-white/0 focus:rounded focus:outline-none focus:ring-2 focus:ring-white/55"
         >
           Skip to main content
         </a>
